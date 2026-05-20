99a93d18b61c23616f78301313503ef721a1edc8
Gustavoh345 <gustavohsilva345@gmail.com>
2026-05-19 22:26:10 -0300

    Continuação dos testes do botão/quase finalização deles, criação do beforeEach e utilização do rerender

--- src/components/Button/__tests__/Button.spec.tsx
+++ src/components/Button/__tests__/Button.spec.tsx
@@ -1,26 +1,65 @@
 //este "it" so funciona se estiver sendo realizada uma suite(varios testes), se for apenas 1 teste, usa-se test
-import { describe, test, it, expect } from "vitest";
+import { describe, test, it, expect, vi, beforeEach } from "vitest";
 import { render, screen } from "@testing-library/react";
 import { Button } from "../Button";
+import userEvent from "@testing-library/user-event";
+
 
 describe ("Testes do componente Button", 
 () => {
-    it("Deve chamar a função ao clicar no botão", () => {
+    //variaveis vazias
+    let user, handleClick, renderResult;
 
+    //antes de cada test ele faz isso, pois isso era repetido em todos os testes
+    //serve para renderizar, logo os testes nao sao responsaveis em renderizar componente, mas sim testar ele
+    beforeEach(() => {
+        user = userEvent.setup();
+        handleClick = vi.fn();
+
+        renderResult = <Button onClick={handleClick}>clique aqui</Button>
+    })
+
+    //devido ao await do clique no botao do user, vem este async antes dos parenteses para indicar que é uma funcao async
+    it("Deve chamar a função ao clicar no botão", async () => {
+        
+        const ButtonElement = screen.getByRole("button", {
+            name: /clique aqui/i,
+        });
+
+        await user.click(ButtonElement);
+
+        expect(handleClick).toHaveBeenCalled();
     });
     
-    it("Não deve ser clicável quando estiver desabilitado", () => {
 
+    it("Não deve ser clicável quando estiver desabilitado", async () => {
+
+        renderResult.rerender(
+            <Button onClick={handleClick} disabled>
+                Clique aqui
+            </Button>
+        );
+
+        const ButtonElement = screen.getByRole("button", {
+            name: /clique aqui/i,
+        });
+
+        await user.click(ButtonElement);
+
+        expect(handleClick).not.toHaveBeenCalled();
     });
 
     //verificar se o texto do botao esta aparecendo corretamente
-    it("Deve renderizar o texto corretamente", () => {
-        render(<Button>Salvar perfil</Button>)
+    it("Deve renderizar o texto corretamente", async () => {
+
         const buttonElement = screen.getByRole("button", {
-            name: /salvar perfil/i,
+            name: /clique aqui/i,
         })
 
         expect(buttonElement).toBeInTheDocument();
-        ;
+        
+        await user.click(ButtonElement);
+
+        expect(handleClick).not.toHaveBeenCalled();
     });
 });
